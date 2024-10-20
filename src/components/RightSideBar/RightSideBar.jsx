49f9b788d4dde659d765233d5aed3f38a48cff21
kaa1203 <kennethaguila1203@gmail.com>
2024-10-20 14:54:19 +0800

    Update RightSideBar.jsx

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -11,7 +11,7 @@ export const RightSideBar = () => {
 	const now = `${(today.getDate()).toString().padStart(2, '0')}.${(today.getMonth() + 1).toString().padStart(2, '0')}.${today.getFullYear()}`;
 	const dispatch = useDispatch();
 	
-	const { totalCalories, entry } = useGetEntry();
+	const { totalCalories } = useGetEntry();
 	const { calculation } = useGetCalorieIntake();
 
 	useEffect(() => {
